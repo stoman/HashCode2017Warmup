008c5bc608242f825175ab19b4a00db02af4b6bf
Christian Müller <christian.mueller@tum.de>
2017-02-20 17:54:41 +0100

    input/output

---
+++ visualize/AnswerToGnu.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class AnswerToGnu {
+	public static void main(String[] args) throws FileNotFoundException {
+		// read
+		// System.setIn(new FileInputStream(new File("small.ans")));
+		// System.setOut(new PrintStream(new FileOutputStream(new File("small.ans.dat"))));
+
+		Scanner sc = new Scanner(System.in);
+
+		int s = sc.nextInt();
+
+		for (int i = 1; i <= s; i++) {
+			int y1 = sc.nextInt();
+			int x1 = sc.nextInt();
+			int y2 = sc.nextInt();
+			int x2 = sc.nextInt();
+
+			System.out.println(
+					"set object " + i + " rect from " + (x1 - 0.1) + "," + (y1 - 0.1) + " to " + (x2 + 0.1) + "," + (y2 + 0.1));
+		}
+	}
+}
---
+++ visualize/InputToGnu.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class InputToGnu {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		// read
+		// System.setIn(new FileInputStream(new File("small.in")));
+		// System.setOut(new PrintStream(new FileOutputStream(new File("small.dat"))));
+
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		int c = sc.nextInt();
+		int l = sc.nextInt();
+		int h = sc.nextInt();
+		sc.nextLine();
+
+		char[][] grid = new char[r][c];
+
+		for (int i = 0; i < r; i++) {
+			String line = sc.nextLine();
+			for (int j = 0; j < c; j++) {
+				grid[i][j] = line.charAt(j);
+			}
+		}
+		
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				// if (i > 10 && i < 40 && j > 0 && j < 35)
+				System.out.println(j + " " + i + " " + (grid[i][j] == 'T' ? 1 : 2));
+			}
+		}
+	}
+
+}
