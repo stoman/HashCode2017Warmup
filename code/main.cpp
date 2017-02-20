e3545bc78936320a18174e3eda7d0801d2e80d12
Gregor Schwarz <gregor.schwarz@tum.de>
2017-02-20 17:02:39 +0100

    Splitted checkSlice function for horizontal and vertical version

--- code/main.cpp
+++ code/main.cpp
@@ -19,7 +19,7 @@ struct Output {
 };
 
 
-bool checkSlice(Input &input, Slice &s)
+bool checkSliceHorizontal(Input &input, Slice &s)
 {
 	bool t = false;
 	bool f = false;
@@ -36,6 +36,25 @@ bool checkSlice(Input &input, Slice &s)
 	return false;
 }
 
+
+bool checkSliceVertical(Input &input, Slice &s)
+{
+	bool t = false;
+	bool f = false;
+	for (int i = s.r1; i <= s.r2; i++)
+	{
+		if (input.tomatoes[i][s.c1])
+			t = true;
+		else
+			f = true;
+
+		if (t == true && f == true)
+			return true;
+	}
+	return false;
+}
+
+
 void solveSimpleHorizontal(Input& input, Output& output) {
 	for (int i = 0; i < input.r; i++)
 	{
@@ -46,7 +65,7 @@ void solveSimpleHorizontal(Input& input, Output& output) {
 			s.c1 = j;
 			s.r2 = i;
 			s.c2 = min(j+input.h-1, input.c-1);
-			if (checkSlice(input, s))
+			if (checkSliceHorizontal(input, s))
 				output.slices.push_back(s);
 		}
 	}
@@ -62,7 +81,7 @@ void solveSimpleVertical(Input& input, Output& output) {
 			s.c1 = j;
 			s.r2 = min(i+input.h-1, input.r-1);
 			s.c2 = j;
-			if (checkSlice(input, s))
+			if (checkSliceVertical(input, s))
 				output.slices.push_back(s);
 		}
 	}
