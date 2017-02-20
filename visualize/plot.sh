4c876b6743d9981a6d33606d3ba99d18a3222dc6
Christian Müller <christian.mueller@tum.de>
2017-02-20 18:07:25 +0100

    paths'

--- visualize/plot.sh
+++ visualize/plot.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-FILENAME=$1
+FILENAME1=$1
+FILENAME2=$2
 
 if [ "$FILENAME" != "" ]; then
-    gnuplot -e "datafile='${FILENAME}.in.dat';datafile2='${FILENAME}.ans.dat'; outputname='${FILENAME}.png'" plot.plg
+    gnuplot -e "datafile='${FILENAME1}';datafile2='${FILENAME2}'; outputname='${FILENAME1}.png'" plot.plg
 else
     echo "Please enter a filename"
 fi
