841a3ca5f5e1904fb0b8cdfa53708109b559646b
Stefan Toman <git@stoman.de>
2017-03-13 21:48:01 +0100

    add input parsing

--- satellites/code/util.cpp
+++ satellites/code/util.cpp
@@ -15,9 +15,12 @@ using namespace std;
 
 //util classes
 struct Object {
-	ll lon, lat;
+	ll lat, lon;
 	bool done;
 	vector<int> collections;
+
+	Object(ll lat, ll lon): lat(lat), lon(lon), done(false) {};
+	Object() {};
 };
 
 struct Collection {
@@ -26,11 +29,15 @@ struct Collection {
 	bool done;
 	vector<Object> objects;
 	vector<pair<ll, ll>> time_ranges;
+
+	Collection(ll v, ll l, ll r, int id): v(v), l(l), r(r), id(id), done(false) {};
 };
 
 struct Satellite {
-	ll lon, lat, v, w, d;
+	ll lat, lon, v, w, d;
 	map<ll, Object> photos;
+
+	Satellite(ll lat, ll lon, ll v, ll w, ll d): lat(lat), lon(lon), v(v), w(w), d(d) {};
 };
 
 struct Input {
@@ -38,9 +45,34 @@ struct Input {
 	int s, c;
 	vector<Satellite> satellites;
 	vector<Collection> collections;
+	map<pair<ll, ll>, Object> objects;
 };
 
 //input handling
 void readInput(Input& input, istream& in) {
-	//TODO read input
+	in >> input.t >> input.s;
+	for(int i = 0; i < input.s; i++) {
+		ll lat, lon, v, w, d;
+		in >> lat >> lon >> v >> w >> d;
+		input.satellites.push_back(*(new Satellite(lat, lon, v, w, d)));
+	}
+	in >> input.c;
+	for(int i = 0; i < input.c; i++) {
+		ll v, l, r;
+		in >> v >> l >> r;
+		input.collections.push_back(*(new Collection(v, l, r, i)));
+		for(int j = 0; j < l; j++) {
+			ll lat, lon;
+			in >> lat >> lon;
+			if(input.objects.find(make_pair(lat, lon)) == input.objects.end()) {
+				input.objects[make_pair(lat, lon)] = *(new Object(lat, lon));
+			}
+			input.objects[make_pair(lat, lon)].collections.push_back(i);
+		}
+		for(int j = 0; j < l; j++) {
+			ll ts, te;
+			in >> ts >> te;
+			input.collections[i].time_ranges.push_back(make_pair(ts, te));
+		}
+	}
 }
