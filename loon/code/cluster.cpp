846a5d5302a0c80a44565b052106b21f72f88d79
Stefan Toman <git@stoman.de>
2017-03-20 20:54:45 +0100

    add recompute centers of clusters

--- loon/code/cluster.cpp
+++ loon/code/cluster.cpp
@@ -3,6 +3,8 @@
 
 void cluster(Input& input) {
 	int n = input.b;
+
+	//first bad matching
 	input.clusters.resize(n);
 	for(int i = 0; i < input.l; i++) {
 		if(i < n) {
@@ -11,5 +13,16 @@ void cluster(Input& input) {
 			input.clusters[i].center_c = input.cell_c[i];
 		}
 		input.clusters[i%n].cells.push_back(make_pair(input.cell_r[i], input.cell_c[i]));
+	}
+}
+
+void recompute_centers(Input& input) {
+	for(Cluster& cluster: input.clusters) {
+		cluster.center_r = .0;
+		cluster.center_c = .0;
+		for(pair<int, int>& cell: clusters.cells) {
+			cluster.center_r += 1.*cell.first/cluster.cells.size();
+			cluster.center_c += 1.*cell.second/cluster.cells.size();
+		}
 	}
 }
