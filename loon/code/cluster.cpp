6aceb76e3b64ab5e86f58988a1d37025385bc52c
Stefan Toman <git@stoman.de>
2017-03-20 22:05:15 +0100

    finish kmeans algorithm

--- loon/code/cluster.cpp
+++ loon/code/cluster.cpp
@@ -10,13 +10,13 @@ void reassign_cells(Input& input) {
 		cluster.cells.clear();
 	}
 	for(int i = 0; i < input.l; i++) {
-		Cluster& best = input.clusters[0];
+		Cluster* best = &input.clusters[0];
 		for(Cluster& cluster: input.clusters) {
-			if(dist(best, input.cell_r[i], input.cell_c[i]) > dist(cluster, input.cell_r[i], input.cell_c[i])) {
-				cluster = best;
+			if(dist(*best, input.cell_r[i], input.cell_c[i]) > dist(cluster, input.cell_r[i], input.cell_c[i])) {
+				best = &cluster;
 			}
 		}
-		best.cells.push_back(make_pair(input.cell_r[i], input.cell_c[i]));
+		best->cells.push_back(make_pair(input.cell_r[i], input.cell_c[i]));
 	}
 }
 
@@ -28,31 +28,64 @@ void recompute_centers(Input& input) {
 			cluster.center_r += 1.*cell.first/cluster.cells.size();
 			cluster.center_c += 1.*cell.second/cluster.cells.size();
 		}
+		if(cluster.center_c == .0) {
+			int r = rand() % input.l;
+			cluster.center_r = input.cell_r[r];
+			cluster.center_c = input.cell_c[r];
+		}
+	}
+}
+
+void debug_clusters(Input& input) {
+	for(Cluster& cluster: input.clusters) {
+		for(pair<int, int>& cell: cluster.cells) {
+			cerr << cell.second << " " << cell.first << " " << 1.*cluster.id/input.b << endl;
+		}
 	}
 }
 
 void cluster(Input& input) {
-	int n = input.b;
-	int m = 50;
-
 	cerr << "start clustering" << endl;
 
-	//first bad matching
+	int n = input.b;
+	int m = 500;
 	input.clusters.resize(n);
-	for(int i = 0; i < input.l; i++) {
+
+	//first bad matching
+	/*for(int i = 0; i < input.l; i++) {
 		if(i < n) {
 			input.clusters[i].id = i;
 			input.clusters[i].center_r = input.cell_r[i];
 			input.clusters[i].center_c = input.cell_c[i];
 		}
 		input.clusters[i%n].cells.push_back(make_pair(input.cell_r[i], input.cell_c[i]));
+	}*/
+
+	for(int i = 0; i < n; i++) {
+		//initial clusters
+		int r = rand() % input.l;
+		input.clusters[i].id = i;
+		input.clusters[i].center_r = input.cell_r[r];
+		input.clusters[i].center_c = input.cell_c[r];
+		reassign_cells(input);
 	}
 
 	for(int i = 0; i < m; i++) {
-		if(i % 10 == 0) cerr << "kmeans step " << (i+1) << "/" << m << endl;
+		if(i % 10 == 0) {
+			int debug = 0;
+			for(Cluster& cluster: input.clusters) {
+				if(!cluster.cells.empty()) {
+					debug++;
+				}
+			}
+			cerr << "kmeans step " << (i+1) << "/" << m << ", " << debug << " clusters with cells" << endl;
+		}
 		recompute_centers(input);
 		reassign_cells(input);
+
 	}
+
+	//debug_clusters(input);
 
 	cerr << "clustering finished" << endl;
 }
