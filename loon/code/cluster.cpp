eba2050ed64ec0869171742c0a413f6d9088519a
Stefan Toman <git@stoman.de>
2017-03-20 20:44:40 +0100

    add dumb first version of clusters

--- loon/code/cluster.cpp
+++ loon/code/cluster.cpp
@@ -2,5 +2,14 @@
 #include "util.cpp"
 
 void cluster(Input& input) {
-	//TODO fill me
+	int n = input.b;
+	input.clusters.resize(n);
+	for(int i = 0; i < input.l; i++) {
+		if(i < n) {
+			input.clusters[i].id = i;
+			input.clusters[i].center_r = input.cell_r[i];
+			input.clusters[i].center_c = input.cell_c[i];
+		}
+		input.clusters[i%n].cells.push_back(make_pair(input.cell_r[i], input.cell_c[i]));
+	}
 }
