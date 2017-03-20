cde9108fbc8d516b03262a88df05d0687ffd7569
Gregor Schwarz <gregor.schwarz@tum.de>
2017-03-20 20:59:13 +0100

    Read Input

--- loon/code/util.cpp
+++ loon/code/util.cpp
@@ -16,7 +16,8 @@ using namespace std;
 
 //util classes
 struct Cluster {
-	int center_r, center_c, id;
+	double center_r, center_c;
+	int id;
 	vector<pair<int, int>> cells;
 };
 
@@ -37,8 +38,47 @@ struct Input {
 
 //input handling
 void readInput(Input& input, istream& in) {
-	
-	
+	in >> input.r >> input.c >> input.a;
+	in >> input.l >> input.v >> input.b >> input.t;
+	in >> input.rs >> input.cs;
+	for (int i = 0; i < input.l; ++i)
+	{
+		int ri, ci;
+		in >> ri >> ci;
+		input.cell_r.push_back(ri);
+		input.cell_c.push_back(ci);
+	}
+
+
+	// resize movement
+	input.movement_r.resize(input.r);
+	input.movement_c.resize(input.r);
+	for (int i = 0; i < input.r; ++i)
+	{
+		input.movement_r[i].resize(input.c);
+		input.movement_c[i].resize(input.c);
+
+		for (int j = 0; j < input.c; ++j)
+		{
+			input.movement_r[i][j].resize(input.a);
+			input.movement_c[i][j].resize(input.a);
+		}
+	}
+
+
+	for (int a = 0; a < input.a; ++a)
+	{
+		for (int r = 0; r < input.r; ++r)
+		{
+			for (int c = 0; c < input.c; ++c)
+			{
+				int dr, dc;
+				in >> dr >> dc;
+				input.movement_r[r][c][a] = dr;
+				input.movement_c[r][c][a] = dc;
+			}
+		}
+	}
 }
 
 
