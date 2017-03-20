c2b2d52a150b091c2a42d8839ccf9d23df05d893
Stefan Toman <git@stoman.de>
2017-03-20 21:09:36 +0100

    add reassign cells function

--- loon/code/cluster.cpp
+++ loon/code/cluster.cpp
@@ -1,6 +1,37 @@
 #pragma once
 #include "util.cpp"
 
+ll dist(Cluster& cluster, int cell_r, int cell_c) {
+	return ((ll) (cluster.center_r - cell_r)) * ((ll) (cluster.center_r - cell_r)) + ((ll) (cluster.center_c - cell_c)) * ((ll) (cluster.center_c - cell_c));
+}
+
+void reassign_cells(Input& input) {
+	for(Cluster& cluster: input.clusters) {
+		cluster.cells.clear();
+	}
+	for(int i = 0; i < input.l; i++) {
+		Cluster& best = input.clusters[0];
+		for(Cluster& cluster: input.clusters) {
+			if(dist(best, input.cell_r[i], input.cell_c[i]) > dist(cluster, input.cell_r[i], input.cell_c[i])) {
+				cluster = best;
+			}
+		}
+		best.cells.push_back(make_pair(input.cell_r[i], input.cell_c[i]));
+	}
+}
+
+void recompute_centers(Input& input) {
+	for(Cluster& cluster: input.clusters) {
+		cluster.center_r = .0;
+		cluster.center_c = .0;
+		for(pair<int, int>& cell: cluster.cells) {
+			cluster.center_r += 1.*cell.first/cluster.cells.size();
+			cluster.center_c += 1.*cell.second/cluster.cells.size();
+		}
+	}
+	reassign_cells(input);
+}
+
 void cluster(Input& input) {
 	int n = input.b;
 
@@ -13,16 +44,5 @@ void cluster(Input& input) {
 			input.clusters[i].center_c = input.cell_c[i];
 		}
 		input.clusters[i%n].cells.push_back(make_pair(input.cell_r[i], input.cell_c[i]));
-	}
-}
-
-void recompute_centers(Input& input) {
-	for(Cluster& cluster: input.clusters) {
-		cluster.center_r = .0;
-		cluster.center_c = .0;
-		for(pair<int, int>& cell: clusters.cells) {
-			cluster.center_r += 1.*cell.first/cluster.cells.size();
-			cluster.center_c += 1.*cell.second/cluster.cells.size();
-		}
 	}
 }
