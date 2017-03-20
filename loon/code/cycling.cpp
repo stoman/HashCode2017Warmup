b072960a49097289250956299d4cdec6c8a14d7e
Christian Müller <christian.mueller@tum.de>
2017-03-20 22:13:52 +0100

    test

--- loon/code/cycling.cpp
+++ loon/code/cycling.cpp
@@ -4,15 +4,21 @@
 #include <set>
 #include <tuple>
 
-long id(int r, int c, int h) {
-    return r + 1000 * c + 100000 * h;
+long id(int r, int c, int a) {
+    return r + 10000 * c + 1000000 * a;
+}
+
+bool inbounds(int r, int c, Input& input) {
+    return (0 <= r && r < input.r && 0 <= c && c < input.c);
 }
 
 int tr_r(int r, int c, int a, Input& input) {
+    // cerr << "translating " << r << " " << c << " " << a << endl;
     return r + input.movement_r[r][c][a];
 }
 int tr_c(int r, int c, int a, Input& input) {
-    return c + input.movement_c[r][c][a];
+    // cerr << "translating " << r << " " << c << " " << a << endl;
+    return (c + input.movement_c[r][c][a] + input.c) % input.c;
 }
 
 void cycling(Input& input) {
@@ -48,35 +54,54 @@ void cycling(Input& input) {
             q_a.pop();
             q_l.pop();
 
+            // cerr << "p(" << r << "," << c << "," << a << "," << l << ")" << endl;
+
             long i = id(r, c, a);
-            if (find(seen.begin(), seen.end(), i) == seen.end()) {
+            if (seen.find(i) == seen.end()) {
                 seen.insert(i);
             } else {
-                cerr << "cycle of len " << (len - l);
+                cerr << "cycle of len " << (len - l) << endl;
             }
 
             if (l <= 0) {
                 continue;
             }
 
+            // cerr << "one";
+
             // reachable:
             if (a > 1) {
-                q_r.push(tr_r(r, c, a - 1, input));
-                q_c.push(tr_c(r, c, a - 1, input));
-                q_a.push(a - 1);
+                int rn = tr_r(r, c, a - 1, input);
+                int cn = tr_c(r, c, a - 1, input);
+                if (inbounds(rn, cn, input)) {
+                    q_r.push(rn);
+                    q_c.push(cn);
+                    q_a.push(a - 1);
+                    q_l.push(l - 1);
+                }
+            }
+            if (a < input.a) {
+                int rn = tr_r(r, c, a + 1, input);
+                int cn = tr_c(r, c, a + 1, input);
+                if (inbounds(rn, cn, input)) {
+                    q_r.push(rn);
+                    q_c.push(cn);
+                    q_a.push(a + 1);
+                    q_l.push(l - 1);
+                }
+            }
+            // cerr << "two";
+
+            int rn = tr_r(r, c, a, input);
+            int cn = tr_c(r, c, a, input);
+            if (inbounds(rn, cn, input)) {
+                q_r.push(rn);
+                q_c.push(cn);
+                q_a.push(a);
                 q_l.push(l - 1);
             }
-            if (a < input.a - 1) {
-                q_r.push(tr_r(r, c, a + 1, input));
-                q_c.push(tr_c(r, c, a + 1, input));
-                q_a.push(a + 1);
-                q_l.push(l - 1);
-            }
-            q_r.push(tr_r(r, c, a, input));
-            q_c.push(tr_c(r, c, a, input));
-            q_a.push(a);
-            q_l.push(l - 1);
         }
     }
+    cerr << "end Emi" << endl;
 
 }
