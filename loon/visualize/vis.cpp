0706822ea4527134130ab53294901686ad99c1d6
Christian Müller <christian.mueller@tum.de>
2017-03-22 21:16:03 +0100

    better vis

--- loon/visualize/vis.cpp
+++ loon/visualize/vis.cpp
@@ -1,6 +1,7 @@
 #include "../code/util.cpp"
 #include <iostream>
 #include <fstream>
+#include <set>
 
 //input/output code
 int main(int argc, char* argv[]) {
@@ -22,31 +23,37 @@ int main(int argc, char* argv[]) {
     //     input.balloons[i].h[0] = 0;
     // }
 
+    set<int> losts;
+
     for (int tt = 0; tt < input.t; tt++) {
         // cerr << "time " << tt << endl;
         for (int i = 0; i < input.b; i++) {
+            if (losts.find(i) != losts.end()) {
+                continue;
+            }
+
             int da;
             ans >> da;
 
             int rc = input.balloons[i].r[tt];
             int cc = input.balloons[i].c[tt];
             int ac = input.balloons[i].h[tt];
-
-
             int an = ac + da;
-            // cerr << "loon " << i << " starting " << rc << " " << cc << " " << ac << endl;
-            int rn = rc + input.movement_r[rc][cc][an];
-            int cn = cc + input.movement_c[rc][cc][an];
+
+            if (rc < 0 || input.r <= rc || an < 0 || an > input.a) {
+                cerr << "loon " << i << " lost at " << rc << " " << cc << " " << an << endl;
+                losts.insert(i);
+                continue;
+            }
+
+            int rn = rc;
+            int cn = cc ;
+            if (an != 0) {
+                rn += input.movement_r[rc][cc][an];
+                cn += input.movement_c[rc][cc][an];
+            }
 
             cout << cc << ' ' << rc << ' ' << cn-cc << ' ' << rn-rc << ' ' << i << endl;
-            // << input.balloons[balloon].cluster_id / ((double)input.clusters.size()) << endl;
-
-            if (rn < 0 || input.r <= rn || an <= 0 || an > input.a) {
-                // cerr << "loon " << i << " lost" << endl;
-                rn = rc;
-                cn = cc;
-                an = ac;
-            }
             cn = (cn + input.c) % input.c;
 
             if (tt + 1 < input.t) {
